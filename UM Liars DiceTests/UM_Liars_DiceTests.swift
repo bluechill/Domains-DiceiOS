50489bb5e19a4339c897b078bee298fe325e38fe
Alex Turner <bluechill@me.com>
2016-05-15 12:14:14 -0400

    Initial Commit

---
+++ UM Liars DiceTests/UM_Liars_DiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  UM_Liars_DiceTests.swift
+//  UM Liars DiceTests
+//
+//  Created by Alex Turner on 5/15/16.
+//  Copyright © 2016 Alex Turner. All rights reserved.
+//
+
+import XCTest
+@testable import UM_Liars_Dice
+
+class UM_Liars_DiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
