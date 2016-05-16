44968d5a7bd7ae1ab5e23b3fd421a90489a366ed
Alex Turner <bluechill@me.com>
2016-05-16 11:20:41 -0400

    BidAction + tests

--- DiceLogicEngine/History/BidAction.swift
+++ DiceLogicEngine/History/BidAction.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+import MessagePack
+
+public class BidAction: PushAction
+{
+    static private let countKey: Int = PushAction.pushMaxKey+1
+    static private let faceKey: Int = PushAction.pushMaxKey+2
+    
+    public private(set) var count: UInt64 = 0
+    public private(set) var face: UInt64 = 0
+    
+    public init(player: String, count: UInt64, face: UInt64, pushedDice: Array<UInt64>, correct: Bool)
+    {
+        super.init(player: player, pushedDice: pushedDice, correct: correct, type: .BidAction)
+        
+        self.count = count
+        self.face = face
+    }
+    
+    required public init?(data: MessagePackValue)
+    {
+        super.init(data: data)
+        
+        let array = data.arrayValue!
+        
+        guard array.count >= PushAction.pushMaxKey+3 else {
+            error("BidAction data must have an array of size \(PushAction.pushMaxKey+3)")
+            return nil
+        }
+        
+        guard let count = array[BidAction.countKey].unsignedIntegerValue else {
+            error("BidAction data must have a count key")
+            return nil
+        }
+        
+        guard let face = array[BidAction.faceKey].unsignedIntegerValue else {
+            error("BidAction data must have a die face key")
+            return nil
+        }
+        
+        self.count = count
+        self.face = face
+    }
+    
+    public override func asData() -> MessagePackValue
+    {
+        var array = super.asData().arrayValue!
+        
+        array.append(.UInt(count))
+        array.append(.UInt(face))
+        
+        return .Array(array)
+    }
+    
+    public override func isEqualTo(item: HistoryItem) -> Bool
+    {
+        guard super.isEqualTo(item) else {
+            return false
+        }
+        
+        guard let item = (item as? BidAction) else {
+            return false
+        }
+        
+        return count == item.count && face == item.face
+    }
+}
---
+++ DiceLogicEngineTests/BidActionTests.swift
@@ -0,0 +1,59 @@
+//
+//  BidActionTests.swift
+//  UM Liars Dice
+//
+//  Created by Alex Turner on 5/15/16.
+//  Copyright © 2016 Alex Turner. All rights reserved.
+//
+
+import XCTest
+import MessagePack
+@testable import DiceLogicEngine
+
+class BidActionTests: XCTestCase
+{
+    func testInitialization()
+    {
+        let item = BidAction(player: "Alice", count: 1, face: 2, pushedDice: [1,2,3], correct: true)
+        
+        XCTAssertTrue(item.count == 1)
+        XCTAssertTrue(item.face == 2)
+    }
+    
+    func testEquality()
+    {
+        let item1 = BidAction(player: "Alice", count: 1, face: 2, pushedDice: [], correct: true)
+        let item2 = BidAction(player: "Alice", count: 1, face: 2, pushedDice: [], correct: true)
+        let item3 = BidAction(player: "Alice", count: 2, face: 2, pushedDice: [], correct: true)
+        let item4 = BidAction(player: "Alice", count: 2, face: 3, pushedDice: [], correct: true)
+        
+        XCTAssertTrue(item1 == item2)
+        XCTAssertFalse(item2 == item3)
+        XCTAssertFalse(item2 == item4)
+        XCTAssertFalse(item3 == item4)
+        
+        let action = PushAction(player: "Alice", pushedDice: [], correct: true, type: .BidAction)
+        XCTAssertFalse(item1 == action)
+        
+        let action2 = HistoryItem(type: .Action)
+        XCTAssertFalse(item1 == action2)
+    }
+    
+    func testSerialization()
+    {
+        let item = BidAction(player: "Alice", count: 1, face: 2, pushedDice: [], correct: true)
+        let item_restore = BidAction(data: item.asData())
+        
+        XCTAssertTrue(item == item_restore)
+        
+        XCTAssertNil(BidAction(data: [.UInt(HistoryItem.HIType.BidAction.rawValue),"Alice",.Bool(false),[]]))
+        XCTAssertNil(BidAction(data: [.UInt(HistoryItem.HIType.BidAction.rawValue),"Alice",.Bool(false),[],1]))
+        XCTAssertNil(BidAction(data: [.UInt(HistoryItem.HIType.BidAction.rawValue),"Alice",.Bool(false),[],-1]))
+        
+        XCTAssertNil(BidAction(data: [.UInt(HistoryItem.HIType.BidAction.rawValue),"Alice",.Bool(false),[],1,-1]))
+        XCTAssertNil(BidAction(data: [.UInt(HistoryItem.HIType.BidAction.rawValue),"Alice",.Bool(false),[],-1,-1]))
+        XCTAssertNil(BidAction(data: [.UInt(HistoryItem.HIType.BidAction.rawValue),"Alice",.Bool(false),[],-1,1]))
+        
+        XCTAssertNotNil(BidAction(data: [.UInt(HistoryItem.HIType.BidAction.rawValue),"Alice",.Bool(false),[],1,2]))
+    }
+}
