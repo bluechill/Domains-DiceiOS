5e82768e9b531421ce75ba38666b9e4c3d8f51f2
Alex Turner <bluechill@me.com>
2016-05-20 12:57:16 -0400

    CollectionType extensions

---
+++ DiceLogicEngine/Extensions/CollectionType.swift
@@ -0,0 +1,33 @@
+//
+//  CollectionType.swift
+//  UM Liars Dice
+//
+//  Created by Alex Turner on 5/19/16.
+//  Copyright © 2016 Alex Turner. All rights reserved.
+//
+
+import Foundation
+
+// From: http://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift
+extension CollectionType {
+    /// Return a copy of `self` with its elements shuffled
+    func shuffle() -> [Generator.Element] {
+        var list = Array(self)
+        list.shuffleInPlace()
+        return list
+    }
+}
+
+extension MutableCollectionType where Index == Int {
+    /// Shuffle the elements of `self` in-place.
+    mutating func shuffleInPlace() {
+        // empty and single-element collections don't shuffle
+        if count < 2 { return }
+        
+        for i in 0..<count - 1 {
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+            guard i != j else { continue }
+            swap(&self[i], &self[j])
+        }
+    }
+}
