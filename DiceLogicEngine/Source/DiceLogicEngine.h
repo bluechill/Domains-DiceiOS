f7ae8febd79f3bb149b5f9043b103f6ed2bfd66f
Alex Turner <bluechill@me.com>
2016-08-26 12:46:52 -0400

    OSX version of Dice Engine for testing + cross platform

--- DiceLogicEngine/Source/DiceLogicEngine.h
+++ DiceLogicEngine/Source/DiceLogicEngine.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Alex Turner. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for DiceLogicEngine.
 FOUNDATION_EXPORT double DiceLogicEngineVersionNumber;
